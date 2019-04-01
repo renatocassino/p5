0c7ab5156d97de143f8296dae5147ea9b46e47c9
Tacnoman <renatocassino@gmail.com>
2019-04-01 01:17:16 -0300

    Solution with bug

--- remendo.js
+++ remendo.js
@@ -16,10 +16,10 @@ const newTire = (index) => {
 }
 
 // retornar o quantos furos foram tapados
-const addTape = (_tire, tapeIndex) => {
+const addTape = (_tire, tapeSize) => {
   const tire = [..._tire]
   const position = tire[0]
-  const newPosition = position + tapes[tapeIndex]
+  const newPosition = position + tapeSize
 
   let counter = 0
   while (counter < tire.length - 1) {
@@ -36,25 +36,33 @@ const addTape = (_tire, tapeIndex) => {
   return -1
 }
 
-const solveTire = (tire, tireSize = 0) => {
-  for (let i = 0; i < tapes.length; i++ ) {
-    const newIndex = addTape(tire, 0)
-    if (newIndex === -1) return tireSize + tapes[i]
+const solveTire = (tire, tireSize = 0, tapeSize) => {
+  debugger
+  const newIndex = addTape(tire, tapeSize)
+  if (newIndex === -1) return tireSize + tapeSize
 
-    tireSize += tapes[i]
-    return solveTire(tire.slice(newIndex), tireSize)
-  }
+  tireSize += tapeSize
+  return Math.min(
+    solveTire(tire.slice(newIndex), tireSize, tapes[0]),
+    solveTire(tire.slice(newIndex), tireSize, tapes[1]),
+  )
 }
 
 const run = () => {
   let i = 0
-  let tapeSize = 0
 
+  let less = null
   while(i < qtGaps) {
-    const sizeTape = solveTire(newTire(i))
-    console.log(sizeTape)
+    const minSize = Math.min(
+      solveTire(newTire(i), 0, tapes[0]),
+      solveTire(newTire(i), 0, tapes[1]),
+    )
+
+    if (less === null || minSize < less) less = minSize
     i++
   }
+
+  console.log(less)
 }
 
 run()
